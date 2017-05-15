fad2a339c987387465c29cad0a74084b77c68e4f
Aaron Jimenez <thecrentist00@gmail.com>
2017-05-15 08:52:29 +0000

    added styling and content to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
+  get  '/home', to: 'static_pages#home'
+  get  '/help', to: 'static_pages#help'
+  get  '/signup',  to: 'users#new'
 
   resources :microposts
   resources :users
